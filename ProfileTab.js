ed29fe26eb3cc08593bac8afdbb9f94c6c8e6f70
Valery Leslie Tanada <99020772+valeryleslie@users.noreply.github.com>
2023-04-15 16:30:18 -0700

    Add files via upload

---
+++ ProfileTab.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+<div class="iphone-13-pro-max-10 screen">
+    <div class="overlap-group1">
+        <h1 class="title plusjakartasans-normal-forest-green-40px">
+            <span class="plusjakartasans-normal-forest-green-40px" >Profile</span>
+        </h1>
+        <img
+            class="bunnylogo"
+            src="https://media.discordapp.net/attachments/1095540680388005959/1096929381433233439/bunny_run.jpg?width=422&height=384"
+        />
+    </div>
+    <div class="overlap-group">
+        <img class="mask-group" src="https://media.istockphoto.com/id/1176363686/vector/smiling-young-asian-girl-profile-avatar-vector-icon.jpg?s=612x612&w=0&k=20&c=QuyZJNKexFQgDPr9u91hKieWKOYbaFxPb0b0gwmd-Lo="/>
+    <div class="raised-20 plusjakartasans-normal-black-20px">
+        <span class="plusjakartasans-normal-black-20px">Raised $20</span>
+    </div>
+</div>
+<div class="overlap-group2">
+    <div class="rectangle-16"></div>
+    <div class="still-run-downtown-la plusjakartasans-bold-green-house-20px">
+        <span class="plusjakartasans-bold-green-house-20px">Still;Run Downtown LA</span>
+    </div>
+    <p class="sun-0514-10-miles-w valign-text-middle plusjakartasans-normal-black-16px">
+        <span
+        ><span class="span0">Sun 05/14 <br />10 miles<br />Westwood, LA<br /></span
+        ><span class="plusjakartasans-normal-black-16px"><br />Host: Still;Run<br />Non-Profit: NAMI<br />Raise $2.40</span>
+        </ span>
+    </p>
+    <img class="mask-group-1" src="https://static.wixstatic.com/media/3cc523_c26e6dd9ca924c5bb08a0c16a603e39a~mv2.jpg/v1/fill/w_760,h_1072,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/3cc523_c26e6dd9ca924c5bb08a0c16a603e39a~mv2.jpg"/>
+    <div class="valerys-schedule plusjakartasans-normal-forest-green-20px">
+        <span class="plusjakartasans-normal-forest-green-20px" >Valery's Schedule</span>
+    </div>
+</div>
+<div class="flex-row">
+    <img class="image-3" src="https://cdn.animaapp.com/projects/643b242f2bf36e699266b10d/releases/643b24f06a5fbd984950d135/img/mask-group-1@2x.png" />
+    <div class="overlap-groups">
+        <img class="person-4" src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"/>
+    </div>
+    </div>
+</div>
